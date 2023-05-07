3a7a3e6a0f4f76960026a0fdf6bc846ab024f01e
Marco Túlio <marcotmotta@gmail.com>
2023-05-06 22:29:57 -0300

    player movement

---
+++ Player/Player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+const SPEED = 20
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var viewport
+
+func _ready():
+	viewport = get_viewport()
+
+func _process(delta):
+	var forward_direction = get_forward_direction()
+	$MeshInstance3D.look_at(forward_direction, Vector3.UP)
+	$CollisionShape3D.look_at(forward_direction, Vector3.UP)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta * 10
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+	print(Engine.get_frames_per_second())
+
+func get_forward_direction():
+	var mouse_pos_2d = viewport.get_mouse_position()
+
+	var ray_origin = $Camera3D.project_ray_origin(mouse_pos_2d)
+	var ray_dir = $Camera3D.project_ray_normal(mouse_pos_2d)
+
+	var t = (position.y - ray_origin.y) / ray_dir.y
+	return ray_origin + ray_dir * t
--- README.md
+++ README.md
@@ -6,3 +6,43 @@ Krauser Ban
 Jojo
 Gado
 Mark
+
+
+var camera_distance = 8
+var camera_height = 4
+
+var camera_pivot
+var camera
+var speed = 100
+
+func _ready():
+	#camera_pivot = get_node("../CameraPivot")
+	camera = $Camera3D
+
+func _physics_process(delta):
+	# Get the player's movement vector
+	var velocity = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		velocity.z -= 1
+	if Input.is_action_pressed("move_back"):
+		velocity.z += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+
+	# Normalize the movement vector and scale it by the player's speed
+	velocity = velocity.normalized() * speed
+
+	# Rotate the player to face the mouse cursor
+	look_at(get_viewport().get_global_mouse_position() - global_transform.origin, Vector3.UP)
+
+	# Move the player using move_and_slide()
+	move_and_slide()
+	#if new_velocity.length_squared() > 0:
+	#	velocity = new_velocity
+
+	# Update the camera position
+	#var target_position = global_transform.origin - global_transform.basis.z * camera_distance + Vector3.UP * camera_height
+	#var camera_position = camera_pivot.global_transform.origin
+	#camera_pivot.global_transform.origin = camera_position.linear_interpolate(target_position, camera_follow_speed * delta)
